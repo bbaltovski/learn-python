4408fff4257487a077183c3fd2adc90708961274
bbaltovski <121245996+bbaltovski@users.noreply.github.com>
2023-02-07 10:20:48 -0500

    Update shipping.py

--- 2-control-flow/sals-shipping/shipping.py
+++ 2-control-flow/sals-shipping/shipping.py
@@ -1,36 +1,26 @@
-# Sal's Shipping
-# Sonny Li
-
-weight = 80
-
-# Ground Shipping 🚚
-
+weight = 41.5
+#ground shipping
 if weight <= 2:
   cost_ground = weight * 1.5 + 20
-elif weight <= 6:
-  cost_ground = weight * 3.00 + 20
-elif weight <= 10:
-  cost_ground = weight * 4.00 + 20
-else:
+elif weight >2 and weight <= 6:
+  cost_ground = weight * 3.0 + 20
+elif weight >6 and weight <= 10:
+  cost_ground = weight * 4.0 + 20
+elif weight > 10:
   cost_ground = weight * 4.75 + 20
 
-print("Ground Shipping $", cost_ground)
-      
-# Ground Shipping Premimum 🚚💨
+print(cost_ground)
+cost_premium = 125.00
+print(cost_premium)
 
-cost_ground_premium = 125.00
-
-print("Ground Shipping Premimium $", cost_ground_premium)
-
-# Drone Shipping 🛸
-
+#drone shipping
 if weight <= 2:
   cost_drone = weight * 4.5
-elif weight <= 6:
-  cost_drone = weight * 9.00
-elif weight <= 10:
-  cost_drone = weight * 12.00
-else:
-  cost_drone = weight * 14.25
+elif weight > 2 and weight <=6:
+  cost_drone = weight * 9.0
+elif weight > 6 and weight <= 10:
+  cost_drone = weight * 12.0
+elif weight > 10:
+  cost_drone = weight * 12.25
 
-print("Drone Shipping: $", cost_drone)
+print(cost_drone)
